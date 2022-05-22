cde64513401de777b7e3f415044b9dfbc9a46ca6
Richard Przybyla <95006339+Richlorr@users.noreply.github.com>
2022-05-22 15:19:31 -0600

    Revert "Update GuiTextTranslator.py"
    This reverts commit df299f0193d852a980e5db143f8eef5d6ca64423.

--- GuiTextTranslator.py
+++ GuiTextTranslator.py
@@ -1,11 +1,7 @@
 import time
+import cv2
 import numpy as np
 from mss import mss
-import cv2
-import pytesseract as pt
-from pytesseract import Output
-
-pt.get_languages()
 
 start_time = time.time()
 mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
@@ -18,12 +14,6 @@ with mss() as sct:
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-
-
-
-
-def read_img(img):
-    
 
 # # The simplest use, save a screen shot of the 1st monitor
 # with mss() as sct:
