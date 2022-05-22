9caecca658809c149fcfb5d293034aeb78ac513b
Richard Przybyla <95006339+Richlorr@users.noreply.github.com>
2022-05-22 16:08:14 -0600

    tested pytesseract.image_to_string(...) successfully

--- GuiTextTranslator.py
+++ GuiTextTranslator.py
@@ -7,23 +7,28 @@ from pytesseract import Output
 
 pt.get_languages()
 
-start_time = time.time()
-mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 with mss() as sct:
-    while True:
-        last_time = time.time()
-        img = sct.grab(mon)
-        print('The loop took: {0}'.format(time.time()-last_time))
-        cv2.imshow('test', np.array(img))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    img = sct.shot()
+    string = pt.image_to_string(img)
+
+print(string)
 
 
+# #     VVVV Continuously capture screen VVVV
+# start_time = time.time()
+# mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+# with mss() as sct:
+#     while True:
+#         last_time = time.time()
+#         img = sct.grab(mon)
+#         print('The loop took: {0}'.format(time.time()-last_time))
+#         cv2.imshow('test', np.array(img))
+
+#         if cv2.waitKey(25) & 0xFF == ord('q'):
+#             cv2.destroyAllWindows()
+#             break
 
 
-def read_img(img):
-    
 
 # # The simplest use, save a screen shot of the 1st monitor
 # with mss() as sct:
